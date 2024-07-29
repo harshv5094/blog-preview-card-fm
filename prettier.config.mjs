094da1cf630eda2fe30625066f12f242fbce5c83
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-07-29 12:28:06 +0530

    feat(*): initial Commit

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**@type {import("prettier").Config}*/
+
+const config = {
+  tabWidth: 2,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf'
+}
+
+export default config
